4fba49adcd09d21709b7fdd34560a9845a3780f2
cheng <792702352@qq.com>
2018-08-28 17:35:21 +0800

    重构用户名密码登录

--- cheng-security-app/src/main/java/com/cheng/security/app/ChengResourceServerConfig.java
+++ cheng-security-app/src/main/java/com/cheng/security/app/ChengResourceServerConfig.java
@@ -1,7 +1,16 @@
 package com.cheng.security.app;
 
+import com.cheng.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.cheng.security.core.properties.SecurityConstants;
+import com.cheng.security.core.properties.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 /**
  * 资源服务器配置
@@ -11,5 +20,65 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableResourceServer
-public class ChengResourceServerConfig {
+public class ChengResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private SpringSocialConfigurer chengSpringSocialConfig;
+
+    @Autowired
+    private AuthenticationSuccessHandler chengAuthenticationSuccessHandler;
+
+    @Autowired
+    private AuthenticationFailureHandler chengAuthenticationFailureHandler;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+                .formLogin()
+                // 登录页
+                .loginPage(SecurityConstants.DEFAULT_AUTHENTICATION_URL)
+                // 登录处理页
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                // 成功 handler
+                .successHandler(chengAuthenticationSuccessHandler)
+                // 失败 handler
+                .failureHandler(chengAuthenticationFailureHandler);
+
+        http
+                // 校验码相关配置
+//                .apply(validateCodeSecurityConfig)
+
+//                .and()
+                // 短信登录相关配置
+                .apply(smsCodeAuthenticationSecurityConfig)
+
+                .and()
+                // 社交登录配置
+                .apply(chengSpringSocialConfig)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        SecurityConstants.DEFAULT_AUTHENTICATION_URL,
+                        SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                        securityProperties.getBrowser().getLoginPage(),
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*",
+                        securityProperties.getBrowser().getSignUpUrl(),
+                        securityProperties.getBrowser().getSignOutUrl(),
+                        securityProperties.getBrowser().getSession().getSessionInvalidUrl(),
+                        "/user/register")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+
+                .and()
+                .csrf().disable();
+    }
 }

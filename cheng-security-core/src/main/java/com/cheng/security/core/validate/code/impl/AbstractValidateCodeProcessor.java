0e89aaa39b44c6800d6b3a58b4b34422bc13df7f
cheng <792702352@qq.com>
2018-08-28 22:22:22 +0800

    :sparkles: 重构短信登录 part2

--- cheng-security-core/src/main/java/com/cheng/security/core/validate/code/impl/AbstractValidateCodeProcessor.java
+++ cheng-security-core/src/main/java/com/cheng/security/core/validate/code/impl/AbstractValidateCodeProcessor.java
@@ -3,8 +3,6 @@ package com.cheng.security.core.validate.code.impl;
 import com.cheng.security.core.validate.code.*;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.connect.web.HttpSessionSessionStrategy;
-import org.springframework.social.connect.web.SessionStrategy;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -18,11 +16,6 @@ import java.util.Map;
  *         2018/8/7 14:38
  */
 public abstract class AbstractValidateCodeProcessor<C extends ValidateCode> implements ValidateCodeProcessor {
-
-    /**
-     * 操作 session 的工具类
-     */
-    private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
 
     /**
      * 收集系统中所有的 {@link ValidateCodeGenerator} 接口的实现
@@ -70,7 +63,7 @@ public abstract class AbstractValidateCodeProcessor<C extends ValidateCode> impl
         // 解决验证码存入 session 不能序列化问题
         ValidateCode code = new ValidateCode(validateCode.getCode(), validateCode.getExpireTime());
 //        sessionStrategy.setAttribute(request, getSessionKey(request), code);
-//        validateCodeRepository.save(request, code, getValidateCodeType(request));
+        validateCodeRepository.save(request, code, getValidateCodeType(request));
     }
 
     /**
@@ -86,37 +79,36 @@ public abstract class AbstractValidateCodeProcessor<C extends ValidateCode> impl
     @Override
     public void validate(ServletWebRequest request) {
 
-        ValidateCodeType processorType = getValidateCodeType(request);
-        String sessionKey = getSessionKey(request);
+        ValidateCodeType codeType = getValidateCodeType(request);
 
-        C codeInSession = (C) sessionStrategy.getAttribute(request, sessionKey);
+        C codeInSession = (C) validateCodeRepository.get(request, codeType);
 
         String codeInRequest;
         try {
             codeInRequest = ServletRequestUtils.getStringParameter(request.getRequest(),
-                    processorType.getParamNameOnValidate());
+                    codeType.getParamNameOnValidate());
         } catch (ServletRequestBindingException e) {
             throw new ValidateCodeException("获取验证码的值失败");
         }
 
         if (StringUtils.isBlank(codeInRequest)) {
-            throw new ValidateCodeException(processorType + "验证码的值不能为空");
+            throw new ValidateCodeException(codeType + "验证码的值不能为空");
         }
 
         if (codeInSession == null) {
-            throw new ValidateCodeException(processorType + "验证码不存在");
+            throw new ValidateCodeException(codeType + "验证码不存在");
         }
 
         if (codeInSession.isExpired()) {
-            sessionStrategy.removeAttribute(request, sessionKey);
-            throw new ValidateCodeException(processorType + "验证码已过期");
+            validateCodeRepository.remove(request, codeType);
+            throw new ValidateCodeException(codeType + "验证码已过期");
         }
 
         if (!StringUtils.equals(codeInSession.getCode(), codeInRequest)) {
-            throw new ValidateCodeException(processorType + "验证码不匹配");
+            throw new ValidateCodeException(codeType + "验证码不匹配");
         }
 
-        sessionStrategy.removeAttribute(request, sessionKey);
+        validateCodeRepository.remove(request, codeType);
     }
 
     /**
